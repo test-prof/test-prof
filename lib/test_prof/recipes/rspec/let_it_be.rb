27fe1be724a96fd48fec775ddc3f4346156e4d89
palkan <dementiev.vm@gmail.com>
2017-08-24 15:26:26 +0300

    Add refind option to let_it_be

--- lib/test_prof/recipes/rspec/let_it_be.rb
+++ lib/test_prof/recipes/rspec/let_it_be.rb
@@ -11,30 +11,36 @@ module TestProf
     # And we love cats!)
     PREFIX = "@😸"
 
-    def let_it_be(identifier, reload: nil, &block)
+    def let_it_be(identifier, reload: false, refind: false, &block)
       raise ArgumentError, "Block is required!" unless block_given?
 
       this = self
 
+      initializer = proc do
+        this.instance_variable_set(:"#{PREFIX}#{identifier}", this.instance_exec(&block))
+      end
+
       if within_before_all?
-        before(:all) do
-          this.instance_variable_set(:"#{PREFIX}#{identifier}", this.instance_exec(&block))
-        end
+        before(:all, &initializer)
       else
-        before_all do
-          this.instance_variable_set(:"#{PREFIX}#{identifier}", this.instance_exec(&block))
+        before_all(&initializer)
+      end
+
+      let_accessor = accessor = -> { this.instance_variable_get(:"#{PREFIX}#{identifier}") }
+
+      let_accessor = -> { this.instance_variable_get(:"#{PREFIX}#{identifier}")&.reload } if reload
+
+      if refind
+        let_accessor = lambda do
+          record = this.instance_variable_get(:"#{PREFIX}#{identifier}")
+          next unless record.is_a?(::ActiveRecord::Base)
+
+          record.class.find(record.send(record.class.primary_key))
         end
       end
 
-      accessor =
-        if reload
-          -> { this.instance_variable_get(:"#{PREFIX}#{identifier}")&.reload }
-        else
-          -> { this.instance_variable_get(:"#{PREFIX}#{identifier}") }
-        end
-
-      define_singleton_method(identifier) { this.instance_variable_get(:"#{PREFIX}#{identifier}") }
-      let(identifier, &accessor)
+      define_singleton_method(identifier, &accessor)
+      let(identifier, &let_accessor)
     end
   end
 end
