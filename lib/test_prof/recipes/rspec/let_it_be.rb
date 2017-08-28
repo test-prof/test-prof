78e0b9d5a0904bbb4f12e9bca8254f628f383d36
palkan <dementiev.vm@gmail.com>
2017-08-28 22:38:49 +0300

    Fix LetItBe

--- lib/test_prof/recipes/rspec/let_it_be.rb
+++ lib/test_prof/recipes/rspec/let_it_be.rb
@@ -6,18 +6,27 @@ module TestProf
   # Just like `let`, but persist the result for the whole group.
   # NOTE: Experimental and magical, for more control use `before_all`.
   module LetItBe
+    class << self
+      def module_for(group)
+        modules.fetch(group) do
+          Module.new.tap { |mod| group.prepend(mod) }
+        end
+      end
+
+      private
+
+      def modules
+        @modules ||= {}
+      end
+    end
     # Use uniq prefix for instance variables to avoid collisions
     # We want to use the power of Ruby's unicode support)
     # And we love cats!)
     PREFIX = "@😸"
 
-    def let_it_be(identifier, reload: false, refind: false, &block)
-      raise ArgumentError, "Block is required!" unless block_given?
-
-      this = self
-
+    def let_it_be(identifier, **options, &block)
       initializer = proc do
-        this.instance_variable_set(:"#{PREFIX}#{identifier}", this.instance_exec(&block))
+        instance_variable_set(:"#{PREFIX}#{identifier}", instance_exec(&block))
       end
 
       if within_before_all?
@@ -26,20 +35,35 @@ module TestProf
         before_all(&initializer)
       end
 
-      let_accessor = accessor = -> { this.instance_variable_get(:"#{PREFIX}#{identifier}") }
+      define_let_it_be_methods(identifier, **options)
+    end
 
-      let_accessor = -> { this.instance_variable_get(:"#{PREFIX}#{identifier}")&.reload } if reload
+    def define_let_it_be_methods(identifier, reload: false, refind: false)
+      let_accessor = -> { instance_variable_get(:"#{PREFIX}#{identifier}") }
+
+      let_accessor = -> { instance_variable_get(:"#{PREFIX}#{identifier}")&.reload } if reload
 
       if refind
         let_accessor = lambda do
-          record = this.instance_variable_get(:"#{PREFIX}#{identifier}")
+          record = instance_variable_get(:"#{PREFIX}#{identifier}")
           next unless record.is_a?(::ActiveRecord::Base)
 
           record.class.find(record.send(record.class.primary_key))
         end
       end
 
-      define_singleton_method(identifier, &accessor)
+      LetItBe.module_for(self).module_eval do
+        define_method(identifier) do
+          # Trying to detect the context (couldn't find other way so far)
+          if @__inspect_output =~ /\(:context\)/
+            instance_variable_get(:"#{PREFIX}#{identifier}")
+          else
+            # Fallback to let definition
+            super()
+          end
+        end
+      end
+
       let(identifier, &let_accessor)
     end
   end
--- spec/integrations/let_it_be_spec.rb
+++ spec/integrations/let_it_be_spec.rb
@@ -6,6 +6,6 @@ describe "LetItBe" do
   specify "it works" do
     output = run_rspec('let_it_be')
 
-    expect(output).to include("13 examples, 0 failures")
+    expect(output).to include("17 examples, 0 failures")
   end
 end
