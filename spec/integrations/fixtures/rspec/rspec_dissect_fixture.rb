4af35fc46cb93b92b36e89481093b7fc62dc3325
palkan <dementiev.vm@gmail.com>
2017-08-28 15:03:00 +0300

    Add RSpecDissect

---
+++ spec/integrations/fixtures/rspec/rspec_dissect_fixture.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../../../../lib", __FILE__)
+require "active_support"
+require "test-prof"
+
+class Work
+  attr_reader :runned
+
+  def initialize(val = 0.1)
+    @value = val
+    sleep(@value)
+  end
+
+  def run
+    return false if @runned
+    sleep @value
+    @runned = true
+  end
+end
+
+describe "Subject + Before" do
+  subject(:work) { Work.new }
+
+  specify do
+    expect(work).not_to be_nil
+  end
+
+  it "does nothing" do
+    expect(true).to eq true
+  end
+
+  context "with before" do
+    before { work.run }
+
+    specify do
+      expect(work.runned).to eq true
+    end
+  end
+end
+
+describe "Only let" do
+  let(:work) { Work.new }
+  let(:more_work) { Work.new(1) }
+
+  it "does nothing" do
+    expect(true).to eq true
+  end
+
+  it "takes very long" do
+    expect(work).not_to eq more_work
+  end
+end
