1c6d6f21d6778d14f9e623e0f7b32811b2590901
Sergey Ponomarev <me@sergey-ponomarev.ru>
2017-09-15 13:32:50 +0300

    Fix AggregateFailures specs to support Rubocop 0.50 changes, fixate version in gemspec

--- spec/test_prof/cops/rspec/aggregate_failures_spec.rb
+++ spec/test_prof/cops/rspec/aggregate_failures_spec.rb
@@ -7,84 +7,84 @@ describe RuboCop::Cop::RSpec::AggregateFailures, :config do
   subject(:cop) { described_class.new(config) }
 
   it 'rejects two one-liners in a row' do
-    inspect_source(cop, ['context "request" do',
-                         '  it { is_expected.to be_success }',
-                         '  it { expect(response.body).to eq "OK" }',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  it { is_expected.to be_success }',
+                    '  it { expect(response.body).to eq "OK" }',
+                    'end'])
+
     expect(cop.offenses.size).to eq(1)
     expect(cop.messages.first).to eq('Use :aggregate_failures instead of several one-liners.')
   end
 
   it 'rejects two one-liners when blank lines and non-example blocks' do
-    inspect_source(cop, ['context "request" do',
-                         '  let(:user) { create(:user) } ',
-                         '  before { get "/" }',
-                         '',
-                         '  it { is_expected.to be_success }',
-                         '  ',
-                         '      ',
-                         '  it { expect(response.body).to eq "OK" }',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  let(:user) { create(:user) } ',
+                    '  before { get "/" }',
+                    '',
+                    '  it { is_expected.to be_success }',
+                    '  ',
+                    '      ',
+                    '  it { expect(response.body).to eq "OK" }',
+                    'end'])
     expect(cop.offenses.size).to eq(1)
     expect(cop.messages.first).to eq('Use :aggregate_failures instead of several one-liners.')
   end
 
   it 'rejects one-liners with nested context' do
-    inspect_source(cop, ['context "request" do',
-                         '  it { is_expected.to be_success }',
-                         '  it "works" do',
-                         '    expect(subject).to be_ok',
-                         '  end',
-                         '  it { expect(response.body).to eq "OK" }',
-                         '',
-                         '  context "sub-request" do',
-                         '    let(:params) { "?q=1" }',
-                         '    it { is_expected.to be_valid }',
-                         '  end',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  it { is_expected.to be_success }',
+                    '  it "works" do',
+                    '    expect(subject).to be_ok',
+                    '  end',
+                    '  it { expect(response.body).to eq "OK" }',
+                    '',
+                    '  context "sub-request" do',
+                    '    let(:params) { "?q=1" }',
+                    '    it { is_expected.to be_valid }',
+                    '  end',
+                    'end'])
     expect(cop.offenses).to be_empty
   end
 
   it 'accepts single one-liner' do
-    inspect_source(cop, ['context "request" do',
-                         '  it { is_expected.to be_success }',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  it { is_expected.to be_success }',
+                    'end'])
     expect(cop.offenses).to be_empty
   end
 
   it 'accepts one-liners separated by multiliners' do
-    inspect_source(cop, ['context "request" do',
-                         '  it { is_expected.to be_success }',
-                         '  it "works" do',
-                         '    expect(subject).to be_ok',
-                         '  end',
-                         '  it { expect(response.body).to eq "OK" }',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  it { is_expected.to be_success }',
+                    '  it "works" do',
+                    '    expect(subject).to be_ok',
+                    '  end',
+                    '  it { expect(response.body).to eq "OK" }',
+                    'end'])
     expect(cop.offenses).to be_empty
   end
 
   it 'handles edge cases' do
-    inspect_source(cop, ['context "request" do',
-                         '  include_examples "edges"',
-                         '  xdescribe "POST #create" do',
-                         '  end',
-                         '',
-                         '  pending {}',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  include_examples "edges"',
+                    '  xdescribe "POST #create" do',
+                    '  end',
+                    '',
+                    '  pending {}',
+                    'end'])
     expect(cop.offenses).to be_empty
   end
 
   it 'handles edge cases 2' do
-    inspect_source(cop, ['context "request" do',
-                         '  include_examples "edges"',
-                         'end'])
+    inspect_source(['context "request" do',
+                    '  include_examples "edges"',
+                    'end'])
     expect(cop.offenses).to be_empty
   end
 
   describe "#autocorrect" do
     it "corrects two one-liners" do
       new_source = autocorrect_source(
-        cop,
         ['context "request" do',
          '  it { is_expected.to be_success }',
          '  it { expect(response.body).to eq "OK" }',
@@ -103,7 +103,6 @@ describe RuboCop::Cop::RSpec::AggregateFailures, :config do
 
     it 'corrects indented one-liners when blank lines and non-example blocks' do
       new_source = autocorrect_source(
-        cop,
         ['describe "GET #index" do',
          '  context "request" do',
          '    let(:user) { create(:user) } ',
@@ -133,7 +132,6 @@ describe RuboCop::Cop::RSpec::AggregateFailures, :config do
 
     it "corrects several groups" do
       new_source = autocorrect_source(
-        cop,
         [
           'describe "GET #index" do',
           '  context "request" do',
