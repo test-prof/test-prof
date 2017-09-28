c8b174ada9b7f096c0a8bc22658e7267ed13b9e1
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-09-28 15:14:55 +0300

    Simplify spec structure. Add CustomEvents context and proper fixture for factory.create

--- spec/integrations/event_prof_minitest_spec.rb
+++ spec/integrations/event_prof_minitest_spec.rb
@@ -2,54 +2,68 @@
 
 require 'spec_helper'
 
-describe 'EventProf Minitest integration' do
-  context 'Minitest integration' do
-    specify 'with default rank by time', :aggregate_failures do
-      output = run_minitest('event_prof', env: { 'EVENT_PROF' => 'test.event' })
+describe 'EventProf' do
+  specify 'Minitest integration with default rank by time', :aggregate_failures do
+    output = run_minitest('event_prof', env: { 'EVENT_PROF' => 'test.event' })
 
-      expect(output).to include("EventProf results for test.event")
-      expect(output).to match(/Total time: 59:16\.\d{3}/)
-      expect(output).to include("Total events: 10")
+    expect(output).to include("EventProf results for test.event")
+    expect(output).to match(/Total time: 59:16\.\d{3}/)
+    expect(output).to include("Total events: 10")
 
-      expect(output).to include("Top 5 slowest suites (by time):")
-      expect(output).to include("Top 5 slowest tests (by time):")
+    expect(output).to include("Top 5 slowest suites (by time):")
+    expect(output).to include("Top 5 slowest tests (by time):")
 
-      expect(output).to include(
-        "Another something (./event_prof_fixture.rb) – 50:00.000 (3 / 2)\n"\
-        "Something (./event_prof_fixture.rb) – 09:16.100 (7 / 3)"
-      )
+    expect(output).to include(
+      "Another something (./event_prof_fixture.rb) – 50:00.000 (3 / 2)\n"\
+      "Something (./event_prof_fixture.rb) – 09:16.100 (7 / 3)"
+    )
 
-      expect(output).to include(
-        "do very long ...nvokes 3 times (./event_prof_fixture.rb:48) – 50:00.000 (3)\n"\
-        "invokes twice (./event_prof_fixture.rb:28) – 06:20.000 (2)\n"\
-        "invokes many times (./event_prof_fixture.rb:34) – 02:16.000 (4)\n"\
-        "invokes once (./event_prof_fixture.rb:23) – 00:40.100 (1)"
-      )
-    end
-    specify 'with rank by count', :aggregate_failures  do
+    expect(output).to include(
+      "do very long ...nvokes 3 times (./event_prof_fixture.rb:48) – 50:00.000 (3)\n"\
+      "invokes twice (./event_prof_fixture.rb:28) – 06:20.000 (2)\n"\
+      "invokes many times (./event_prof_fixture.rb:34) – 02:16.000 (4)\n"\
+      "invokes once (./event_prof_fixture.rb:23) – 00:40.100 (1)"
+    )
+  end
+  specify 'Minitest integration with rank by count', :aggregate_failures  do
+    output = run_minitest(
+      'event_prof',
+      env: { 'EVENT_PROF' => 'test.event', 'EVENT_PROF_RANK' => 'count' }
+    )
+
+    expect(output).to include("EventProf results for test.event")
+    expect(output).to match(/Total time: 59:16\.\d{3}/)
+    expect(output).to include("Total events: 10")
+
+    expect(output).to include("Top 5 slowest suites (by count):")
+    expect(output).to include("Top 5 slowest tests (by count):")
+
+    expect(output).to include(
+      "Something (./event_prof_fixture.rb) – 09:16.100 (7 / 3)\n"\
+      "Another something (./event_prof_fixture.rb) – 50:00.000 (3 / 2)"
+    )
+
+    expect(output).to include(
+      "invokes many times (./event_prof_fixture.rb:34) – 02:16.000 (4)\n"\
+      "do very long ...nvokes 3 times (./event_prof_fixture.rb:48) – 50:00.000 (3)\n"\
+      "invokes twice (./event_prof_fixture.rb:28) – 06:20.000 (2)\n"\
+      "invokes once (./event_prof_fixture.rb:23) – 00:40.100 (1)\n"\
+    )
+  end
+
+  context 'CustomEvents' do
+    it "works with factory.create" do
       output = run_minitest(
-        'event_prof',
-        env: { 'EVENT_PROF' => 'test.event', 'EVENT_PROF_RANK' => 'count' }
+        'event_prof_factory_create',
+        env: { 'EVENT_PROF' => 'factory.create' }
       )
 
-      expect(output).to include("EventProf results for test.event")
-      expect(output).to match(/Total time: 59:16\.\d{3}/)
-      expect(output).to include("Total events: 10")
+      expect(output).to include("EventProf results for factory.create")
+      expect(output).to match(/Total time: \d{2}:\d{2}\.\d{3}/)
+      expect(output).to include("Total events: 3")
 
-      expect(output).to include("Top 5 slowest suites (by count):")
-      expect(output).to include("Top 5 slowest tests (by count):")
-
-      expect(output).to include(
-        "Something (./event_prof_fixture.rb) – 09:16.100 (7 / 3)\n"\
-        "Another something (./event_prof_fixture.rb) – 50:00.000 (3 / 2)"
-      )
-
-      expect(output).to include(
-        "invokes many times (./event_prof_fixture.rb:34) – 02:16.000 (4)\n"\
-        "do very long ...nvokes 3 times (./event_prof_fixture.rb:48) – 50:00.000 (3)\n"\
-        "invokes twice (./event_prof_fixture.rb:28) – 06:20.000 (2)\n"\
-        "invokes once (./event_prof_fixture.rb:23) – 00:40.100 (1)\n"\
-      )
+      expect(output).to match(%r{Post \(./event_prof_factory_create_fixture.rb\) – \d{2}:\d{2}.\d{3} \(2 / 1\)})
+      expect(output).to match(%r{User \(./event_prof_factory_create_fixture.rb\) – \d{2}:\d{2}.\d{3} \(1 / 1\)})
     end
   end
 end
---
+++ spec/integrations/fixtures/minitest/event_prof_factory_create_fixture.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../../../../../lib", __FILE__)
+require_relative "../../../support/ar_models"
+require 'minitest/autorun'
+require "test-prof"
+
+describe "Post" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "generates random names" do
+    user2 = FactoryGirl.create(:post).user
+    refute_equal user.name, user2.name
+  end
+end
+
+describe "User" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "validates name" do
+    user.name = ''
+    assert_equal user.valid?, false
+  end
+end
