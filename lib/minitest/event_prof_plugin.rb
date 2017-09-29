ea9251803bd88069ed9cce29c263f9cbc45cb509
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-09-29 08:54:22 +0300

    Add more configuration options to Minitest plugin

--- lib/minitest/event_prof_plugin.rb
+++ lib/minitest/event_prof_plugin.rb
@@ -4,16 +4,27 @@ require 'test_prof/event_prof/minitest'
 
 module Minitest
   def self.plugin_event_prof_options(opts, options)
-    opts.on "--event-prof=EVENT", "Collects metrics for specified EVENT" do |val|
-      options[:event_prof] = val
+    opts.on "--event-prof=EVENT", "Collects metrics for specified EVENT" do |event|
+      options[:event] = event
     end
-    opts.on "--event-prof-rank=RANK_BY", "Defines RANK_BY parameter for results" do |val|
-      options[:event_prof_rank] = val
+    opts.on "--event-prof-rank=RANK_BY", "Defines RANK_BY parameter for results" do |rank|
+      options[:rank_by] = rank.to_sym
     end
+    opts.on "--event-prof-top-count=N", "Limits results with N groups/examples" do |count|
+      options[:top_count] = count.to_i
+    end
+    opts.on "--event-prof-per-example", TrueClass, "Includes examples metrics to results" do |flag|
+      options[:per_example] = flag
+    end
+
   end
 
   def self.plugin_event_prof_init(options)
-    options[:event_prof] = ENV['EVENT_PROF'] if ENV['EVENT_PROF']
-    self.reporter << EventProfReporter.new(options) if options[:event_prof]
+    options[:event] = ENV['EVENT_PROF'] if ENV['EVENT_PROF']
+    options[:rank_by] = ENV['EVENT_PROF_RANK'].to_sym if ENV['EVENT_PROF_RANK']
+    options[:top_count] = ENV['EVENT_PROF_TOP'].to_i if ENV['EVENT_PROF_TOP']
+    options[:per_example] = true if ENV['EVENT_PROF_EXAMPLES']
+
+    self.reporter << EventProfReporter.new(options) if options[:event]
   end
 end
