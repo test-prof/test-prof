aecee66e01300fb354841ca840219ddb192bfc4e
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-10-06 09:33:41 +0300

    Extend Factory Doctor integration spec for Minitest

--- spec/integrations/factory_doctor_spec.rb
+++ spec/integrations/factory_doctor_spec.rb
@@ -3,6 +3,23 @@
 require "spec_helper"
 
 describe "FactoryDoctor" do
+  context "Minitest Integration" do
+    it "works when there are bad examples", :aggregate_failures do
+      output = run_minitest('factory_doctor', env: { 'FDOC' => '1' })
+
+      expect(output).to include("FactoryDoctor report")
+      expect(output).to include("Total (potentially) bad examples: 3")
+      expect(output).to match(/Total wasted time: \d{2}:\d{2}\.\d{3}/)
+
+      expect(output).to include("User (./factory_doctor_fixture.rb:7)")
+      expect(output).to include("generates random names (./factory_doctor_fixture.rb:10) – 2 records created")
+      expect(output).to include("validates name (./factory_doctor_fixture.rb:15) – 1 record created")
+      expect(output).to include("clones (./factory_doctor_fixture.rb:25) – 1 record created")
+      expect(output).not_to include("is ignored")
+      expect(output).not_to include("creates and reloads user")
+    end
+  end
+
   context "RSpec integration" do
     it "works when there are bad examples", :aggregate_failures do
       output = run_rspec('factory_doctor', env: { 'FDOC' => '1' })
