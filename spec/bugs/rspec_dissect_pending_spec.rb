169c822da6ff5f3bff04b657a706f7e7908d101d
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-02-21 13:53:29 +0300

    [Fixes #60] Fix RSpecDissect reporter hooks

---
+++ spec/bugs/rspec_dissect_pending_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+# https://github.com/palkan/test-prof/issues/60
+describe "RSpecDissect with pending examples", type: :integration do
+  specify do
+    output = run_rspec(
+      'rspec_dissect_pending',
+      chdir: File.join(__dir__, "fixtures"),
+      env: { 'RD_PROF' => '1' }
+    )
+
+    expect(output).to include("3 examples, 0 failures, 2 pending")
+    expect(output).to include("RSpecDissect report")
+  end
+end
