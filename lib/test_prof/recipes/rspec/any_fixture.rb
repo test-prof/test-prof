770e63f93143fb9e3eee31d81cde3bff287e989c
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-06-24 12:52:59 +0300

    Fix with_clean_fixture

--- lib/test_prof/recipes/rspec/any_fixture.rb
+++ lib/test_prof/recipes/rspec/any_fixture.rb
@@ -4,6 +4,8 @@ require "test_prof/any_fixture"
 require "test_prof/recipes/rspec/before_all"
 
 RSpec.shared_context "any_fixture:clean", with_clean_fixture: true do
+  extend TestProf::BeforeAll::RSpec
+
   before_all do
     TestProf::AnyFixture.clean
   end
