b3868efccbceb55f913d86fbb1359cf555a68b57
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2018-10-16 22:55:05 +0200

    header with the general information on factories usage

--- lib/test_prof/factory_prof/printers/simple.rb
+++ lib/test_prof/factory_prof/printers/simple.rb
@@ -10,9 +10,17 @@ module TestProf::FactoryProf
           return log(:info, "No factories detected") if result.raw_stats == {}
           msgs = []
 
+          total = result.stats.inject(0) { |acc, stat| acc += stat[:total] }
+          total_top_level = result.stats.inject(0) { |acc, stat| acc += stat[:top_level] }
+          total_uniq_factories = result.stats.map { |stat| stat[:name] }.uniq.count
+
           msgs <<
             <<~MSG
               Factories usage
+
+               Total: #{total}
+               Total top-level: #{total_top_level}
+               Total uniq factories: #{total_uniq_factories}
 
                total      top-level                            name
             MSG
