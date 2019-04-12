aeca405c68507ba3ede13c206455ea8cc7a9283c
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-04-11 22:34:56 -0400

    fix: include_context for older rspecs

--- lib/test_prof/factory_doctor/rspec.rb
+++ lib/test_prof/factory_doctor/rspec.rb
@@ -138,11 +138,12 @@ TestProf.activate('FDOC') do
     config.after(:suite) { listener&.print }
   end
 
-  RSpec.shared_context "factory_doctor:ignore" do
+  RSpec.shared_context "factory_doctor:ignore", fd_ignore: true do
     around(:each) { |ex| TestProf::FactoryDoctor.ignore(&ex) }
   end
 
   RSpec.configure do |config|
+    next unless defined?(config.include_context)
     config.include_context "factory_doctor:ignore", fd_ignore: true
   end
 end
--- lib/test_prof/recipes/rspec/any_fixture.rb
+++ lib/test_prof/recipes/rspec/any_fixture.rb
@@ -3,7 +3,7 @@
 require "test_prof/any_fixture"
 require "test_prof/recipes/rspec/before_all"
 
-RSpec.shared_context "any_fixture:clean" do
+RSpec.shared_context "any_fixture:clean", with_clean_fixture: true do
   extend TestProf::BeforeAll::RSpec
 
   before_all do
@@ -12,7 +12,10 @@ RSpec.shared_context "any_fixture:clean" do
 end
 
 RSpec.configure do |config|
-  config.include_context "any_fixture:clean", with_clean_fixture: true
+  if defined?(config.include_context)
+    config.include_context "any_fixture:clean", with_clean_fixture: true
+  end
+
   config.after(:suite) do
     TestProf::AnyFixture.report_stats if TestProf::AnyFixture.reporting_enabled?
     TestProf::AnyFixture.reset
