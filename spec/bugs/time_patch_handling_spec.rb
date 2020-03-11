0dbafe1072f6531f8df1990d70ad0c38dc3f2986
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-03-11 18:10:00 -0400

    specs(bug): add regression test for #181

--- spec/bugs/time_patch_handling_spec.rb
+++ spec/bugs/time_patch_handling_spec.rb
@@ -2,29 +2,49 @@
 
 # https://github.com/palkan/test-prof/issues/10
 describe "Time.now patching handling (e.g. Timecop)", type: :integration do
-  specify "works with ad-hoc patch" do
-    output = run_rspec(
-      "time_patch",
-      chdir: File.join(__dir__, "fixtures"),
-      env: {"TAG_PROF" => "a"}
-    )
+  context "TestProf.now" do
+    specify "works with ad-hoc patch" do
+      output = run_rspec(
+        "time_patch",
+        chdir: File.join(__dir__, "fixtures"),
+        env: {"TAG_PROF" => "a"}
+      )
 
-    matches = output.match(/x\s+\d{2}\:(\d{2})\.(\d{3})\s+/)
-    s, ms = matches[1].to_i, matches[2].to_i
+      matches = output.match(/x\s+\d{2}\:(\d{2})\.(\d{3})\s+/)
+      s, ms = matches[1].to_i, matches[2].to_i
 
-    expect(s + ms).to be > 0
+      expect(s + ms).to be > 0
+    end
+
+    specify "works with timecop" do
+      output = run_rspec(
+        "timecop",
+        chdir: File.join(__dir__, "fixtures"),
+        env: {"TAG_PROF" => "a"}
+      )
+
+      matches = output.match(/x\s+\d{2}\:(\d{2})\.(\d{3})\s+/)
+      s, ms = matches[1].to_i, matches[2].to_i
+
+      expect(s + ms).to be > 0
+    end
   end
 
-  specify "works with timecop" do
-    output = run_rspec(
-      "timecop",
-      chdir: File.join(__dir__, "fixtures"),
-      env: {"TAG_PROF" => "a"}
-    )
+  # https://github.com/palkan/test-prof/issues/181
+  context "EventProf" do
+    specify "works with timecop" do
+      output = run_rspec(
+        "event_prof_timecop",
+        chdir: File.join(__dir__, "fixtures"),
+        env: {"EVENT_PROF" => "factory.create"}
+      )
 
-    matches = output.match(/x\s+\d{2}\:(\d{2})\.(\d{3})\s+/)
-    s, ms = matches[1].to_i, matches[2].to_i
+      expect(output).to include("EventProf results for factory.create")
 
-    expect(s + ms).to be > 0
+      matches = output.match(/Total time:\s+\d{2}\:(\d{2})\.(\d{3})\s+/)
+      s, ms = matches[1].to_i, matches[2].to_i
+
+      expect(s + ms).to be > 0
+    end
   end
 end
