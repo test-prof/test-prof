1672580795f788091fef7fccbb973922646e43b8
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-03-11 18:18:17 -0400

    fix: use evented subscriber in event_prof
    This allows us to use our clock and not depend on the Time.now monkey-patches

--- lib/test_prof/event_prof/instrumentations/active_support.rb
+++ lib/test_prof/event_prof/instrumentations/active_support.rb
@@ -4,13 +4,31 @@ module TestProf::EventProf
   module Instrumentations
     # Wrapper over ActiveSupport::Notifications
     module ActiveSupport
+      class Subscriber
+        attr_reader :block, :started_at
+
+        def initialize(block)
+          @block = block
+        end
+
+        def start(*)
+          @started_at = TestProf.now
+        end
+
+        def publish(_name, started_at, finished_at, *)
+          block.call(finished_at - started_at)
+        end
+
+        def finish(*)
+          block.call(TestProf.now - started_at)
+        end
+      end
+
       class << self
-        def subscribe(event)
+        def subscribe(event, &block)
           raise ArgumentError, "Block is required!" unless block_given?
 
-          ::ActiveSupport::Notifications.subscribe(event) do |_event, start, finish, *_args|
-            yield (finish - start)
-          end
+          ::ActiveSupport::Notifications.subscribe(event, Subscriber.new(block))
         end
 
         def instrument(event)
