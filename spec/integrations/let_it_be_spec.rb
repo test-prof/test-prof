3c21d29671c31a8aeff809440c798a2567d75e24
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-07-17 11:26:09 +0300

    feat: add default and metadata-based let_it_be modifiers

--- spec/integrations/let_it_be_spec.rb
+++ spec/integrations/let_it_be_spec.rb
@@ -7,6 +7,12 @@ describe "LetItBe" do
     expect(output).to include("0 failures")
   end
 
+  specify "default and metadata modifiers" do
+    output = run_rspec("let_it_be_modifiers")
+
+    expect(output).to include("0 failures")
+  end
+
   specify "it detects state leakages" do
     output = run_rspec("let_it_be_modification_detection")
 
