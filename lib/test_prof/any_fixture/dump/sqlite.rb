71cc14346e6287254edfa8e4a40162f1cc772ba8
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-11-24 17:16:25 +0300

    feat: add uuid support to postgresql dumps

--- lib/test_prof/any_fixture/dump/sqlite.rb
+++ lib/test_prof/any_fixture/dump/sqlite.rb
@@ -1,37 +1,36 @@
 # frozen_string_literal: true
 
+require "test_prof/any_fixture/dump/base_adapter"
+
 module TestProf
   module AnyFixture
     class Dump
-      module SQLite
-        module_function
-
+      class SQLite < BaseAdapter
         def reset_sequence!(table_name, start)
-          conn = ActiveRecord::Base.connection
+          execute <<~SQL.chomp
+            DELETE FROM sqlite_sequence WHERE name=#{table_name}
+          SQL
 
-          conn.execute("DELETE FROM sqlite_sequence WHERE name=#{table_name}")
-          conn.execute <<~SQL.chomp
+          execute <<~SQL.chomp
             INSERT INTO sqlite_sequence (name, seq)
             VALUES (#{table_name}, #{start})
           SQL
         end
 
         def import(path)
-          db = ActiveRecord::Base.connection.pool.spec.config[:database]
+          db = conn.pool.spec.config[:database]
           return false if %r{:memory:}.match?(db)
 
           # Check that sqlite3 is installed
           `sqlite3 --version`
 
-          ActiveRecord::Base.connection.disconnect!
+          while_disconnected do
+            `sqlite3 #{db} < "#{path}"`
+          end
 
-          `sqlite3 #{db} < "#{path}"`
           true
         rescue Errno::ENOENT
           false
-        ensure
-          # Re-connect back
-          ActiveRecord::Base.connection.reconnect!
         end
       end
     end
