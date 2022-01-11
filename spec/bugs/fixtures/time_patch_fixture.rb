bcddd55bb6c4f31da0e473e6177a73d3b3d1d37c
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-01-11 12:59:32 +0300

    spec: fix time helpers loading in Rails 7+

--- spec/bugs/fixtures/time_patch_fixture.rb
+++ spec/bugs/fixtures/time_patch_fixture.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../../../../../lib", __FILE__)
+require "active_support"
 require "active_support/testing/time_helpers"
 require "test-prof"
 
