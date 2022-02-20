405fdf6de7b958ce16c435c4c7fad64cc933d3fe
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2022-02-20 17:19:57 -0600

    Satisfy rubocop

--- spec/integrations/fixtures/rubocop/aggregate_failures_fixture.rb
+++ spec/integrations/fixtures/rubocop/aggregate_failures_fixture.rb
@@ -2,7 +2,9 @@
 
 describe "something" do
   context "many examples" do
-    it { is_expected.to be_ok }
-    it { expect(subject).to_not be_nil }
+    it do
+      is_expected.to be_ok
+      expect(subject).to_not be_nil
+    end
   end
 end
