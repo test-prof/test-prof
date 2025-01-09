8cc5f06cbaa33f76731987d5d828ff1176105d97
Yaroslav Markin <yaroslav@markin.net>
2025-01-09 04:03:24 +0300

    Fix the YJIT spec: only run on rubies with YJIT built in

--- spec/integrations/yjit_spec.rb
+++ spec/integrations/yjit_spec.rb
@@ -10,23 +10,26 @@ describe TestProf::YJIT do
       end
     end
 
-    # Note that we should not really enable YJIT here to test if it detects
-    # actual usage (`RubyVM::YJIT.enabled?`), as it is currently
-    # impossible to programmatically disable it after enabling.
-    context "with YJIT enabled" do
-      specify "warns about using YJIT" do
-        output = run_rspec("dummy", env: {"RUBY_YJIT_ENABLE" => "1"})
+    # Should only Run on Rubies that have YJIT.
+    if defined?(RubyVM) && defined?(RubyVM::YJIT)
+      # Note that we should not really enable YJIT here to test if it detects
+      # actual usage (`RubyVM::YJIT.enabled?`), as it is currently
+      # impossible to programmatically disable it after enabling.
+      context "with YJIT enabled" do
+        specify "warns about using YJIT" do
+          output = run_rspec("dummy", env: {"RUBY_YJIT_ENABLE" => "1"})
 
-        expect(output).to include("YJIT")
-        expect(output).to include("RUBY_YJIT_ENABLE")
-      end
+          expect(output).to include("YJIT")
+          expect(output).to include("RUBY_YJIT_ENABLE")
+        end
 
-      context "when turned off" do
-        specify "does not warn about using YJIT" do
-          output = run_rspec("dummy",
-            env: {"RUBY_YJIT_ENABLE" => "1", "YJIT_PROF" => "0"})
+        context "when turned off" do
+          specify "does not warn about using YJIT" do
+            output = run_rspec("dummy",
+              env: {"RUBY_YJIT_ENABLE" => "1", "YJIT_PROF" => "0"})
 
-          expect(output).to_not include("YJIT")
+            expect(output).to_not include("YJIT")
+          end
         end
       end
     end
