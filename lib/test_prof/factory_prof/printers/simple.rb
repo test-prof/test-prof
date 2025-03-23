ba657037a4441702f1f1bf4779f3fd350cac2cc6
Araslanov Evgeny <araslanov_e_i@list.ru>
2025-03-23 22:45:10 +0500

    Fixed the width of the name column

--- lib/test_prof/factory_prof/printers/simple.rb
+++ lib/test_prof/factory_prof/printers/simple.rb
@@ -19,6 +19,12 @@ module TestProf::FactoryProf
           total_time = result.stats.sum { |stat| stat[:top_level_time] }
           total_uniq_factories = result.stats.map { |stat| stat[:name] }.uniq.count
 
+          table_indent = 3
+          variations_indent = 2
+          max_name_length = result.stats.map { _1[:name].length }.max
+          max_variation_length = result.stats.flat_map { _1[:variations] }.select(&:present?).map { _1[:name].length }.max || 0
+          name_column_length = truncate_names ? 20 : ([max_name_length, max_variation_length].max + variations_indent)
+
           msgs <<
             <<~MSG
               Factories usage
@@ -27,14 +33,24 @@ module TestProf::FactoryProf
                Total top-level: #{total_top_level_count}
                Total time: #{total_time.duration} (out of #{total_run_time.duration})
                Total uniq factories: #{total_uniq_factories}
-
-                 name                    total   top-level     total time      time per call      top-level time
             MSG
+
+          msgs << format(
+            "%#{table_indent}s%-#{name_column_length}s %8s %12s %13s %16s %17s",
+            "", "name", "total", "top-level", "total time", "time per call", "top-level time"
+          )
+          msgs << ""
 
           result.stats.each do |stat|
             next if stat[:total_count] < threshold
 
-            msgs << formatted(3, 20, truncate_names, stat)
+            msgs << formatted(
+              table_indent,
+              name_column_length,
+              truncate_names,
+              stat
+            )
+
             # move other variation ("[...]") to the end of the array
             sorted_variations = stat[:variations].sort_by.with_index do |variation, i|
               (variation[:name] == "[...]") ? stat[:variations].size + 1 : i
@@ -42,7 +58,12 @@ module TestProf::FactoryProf
             sorted_variations.each do |variation_stat|
               next if variation_stat[:total_count] < threshold
 
-              msgs << formatted(5, 18, truncate_names, variation_stat)
+              msgs << formatted(
+                table_indent + variations_indent,
+                name_column_length - variations_indent,
+                truncate_names,
+                variation_stat
+              )
             end
           end
 
@@ -65,7 +86,7 @@ module TestProf::FactoryProf
 
         def format_string(indent_len, name_len, truncate_names)
           name_format = truncate_names ? "#{name_len}.#{name_len}" : name_len.to_s
-          "%-#{indent_len}s%-#{name_format}s %8d %11d %13.4fs %17.4fs %18.4fs"
+          "%#{indent_len}s%-#{name_format}s %8d %12d %12.4fs %15.4fs %16.4fs"
         end
       end
     end
